1122cc6f68543d6f79f621d5fb405af3e140aa6e
sorrel012 <wain1719@gmail.com>
2024-06-28 16:54:23 +0900

    feat: 홈 페이지 프로필 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
 import Image from 'next/image';
+import profileImage from '../../public/profile.png';
+import Link from 'next/link';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className="flex flex-col items-center justify-center gap-1">
+      <Image
+        src={profileImage}
+        alt="profile"
+        width={250}
+        height={250}
+        priority
+        className="mb-2 rounded-full"
+      />
+      <h2 className="text-2xl font-bold">Hi, I'm Hana</h2>
+      <h3 className="text-xl font-bold">Frontend Engineer</h3>
+      <p>안녕하세요, 프론트엔드 개발자 하나입니다.</p>
+      <Link href="/contact" className="bg-lightPink rounded-lg px-2 text-white">
+        Contact Me
+      </Link>
+    </section>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightPink: '#ff92a9',
+      },
+    },
   },
   plugins: [],
 };
