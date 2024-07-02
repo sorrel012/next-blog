0fe7cdd83c56696ad5f7f77a994bd9764f25e69d
sorrel012 <wain1719@gmail.com>
2024-07-02 16:13:39 +0900

    style: 마크다운 뷰어 code 커스텀

--- src/components/posts/MarkdownViewer.tsx
+++ src/components/posts/MarkdownViewer.tsx
@@ -1,9 +1,33 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function MarkdownViewer({ content }) {
   return (
-    <Markdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <Markdown
+      className="prose lg:prose-xl"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || '');
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              PreTag="div"
+              children={String(children).replace(/\n$/, '')}
+              language={match[1]}
+              style={oneDark}
+            />
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {content}
     </Markdown>
   );
