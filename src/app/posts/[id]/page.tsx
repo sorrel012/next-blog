65c54b07516ba988ae3468c2a89708bd1c04d07d
sorrel012 <wain1719@gmail.com>
2024-07-02 16:00:38 +0900

    style: post 상세 페이지 css 추가

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,14 +1,32 @@
 import { getPostDetail } from '@/service/posts';
 import MarkdownViewer from '@/components/posts/MarkdownViewer';
+import Image from 'next/image';
+import { AiTwotoneCalendar } from 'react-icons/ai';
 
 export default async function PostDetail({ params: { id } }) {
-  const post = await getPostDetail(id);
+  const { title, description, date, path, content } = await getPostDetail(id);
   return (
     <article className="mb-10 px-14">
-      <h1 className="mb-10 border-b-2 border-solid border-b-blue-700 text-3xl font-bold text-blue-800">
-        {post.title}
-      </h1>
-      <MarkdownViewer content={post.content} />
+      <Image
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+        className="mb-10 w-full"
+      />
+      <section>
+        <div className="flex items-center justify-end text-neutral-600">
+          <AiTwotoneCalendar />
+          <p>{date.toString()}</p>
+        </div>
+        <h1 className="border-t-2 border-solid border-b-blue-700 pt-2 text-3xl font-bold text-blue-800">
+          {title}
+        </h1>
+        <p className="mb-10 mt-2 border-b-2 border-solid border-b-blue-700 text-blue-950">
+          {description}
+        </p>
+        <MarkdownViewer content={content} />
+      </section>
     </article>
   );
 }
