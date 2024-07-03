2e0e3fb2864790c6d3a852b34b677767c8429874
sorrel012 <wain1719@gmail.com>
2024-07-03 14:56:25 +0900

    feat: contact form 구현

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,3 +1,85 @@
+'use client';
+
+import { ChangeEvent, FormEvent, useState } from 'react';
+import Banner, { BannerData } from '@/components/contact/Banner';
+
+interface Form {
+  from: string;
+  subject: string;
+  message: string;
+}
+
+const LABEL = 'font-bold';
+const INPUT = 'mb-2 text-black';
+
 export default function ContactForm() {
-  return <></>;
+  const [form, setForm] = useState<Form>({
+    from: '',
+    subject: '',
+    message: '',
+  });
+  const [banner, setBanner] = useState<BannerData | null>(null);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+    setBanner({ message: '성공', state: 'success' });
+    // setTimeout(() => {
+    //   setBanner(null);
+    // }, 3000);
+  };
+
+  return (
+    <>
+      {banner && <Banner banner={banner} />}
+      <form
+        onSubmit={onSubmit}
+        className="m-4 flex w-full max-w-md flex-col gap-2 rounded-xl bg-neutral-600 p-4 text-white shadow-xl"
+      >
+        <label htmlFor="from" className={LABEL}>
+          Your Email
+        </label>
+        <input
+          type="email"
+          id="from"
+          name="from"
+          required
+          autoFocus
+          value={form.from}
+          onChange={onChange}
+          className={INPUT}
+        />
+        <label htmlFor="subject" className={LABEL}>
+          Subject
+        </label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          required
+          value={form.subject}
+          onChange={onChange}
+          className={INPUT}
+        />
+        <label htmlFor="message" className={LABEL}>
+          Message
+        </label>
+        <textarea
+          rows={10}
+          id="message"
+          name="message"
+          required
+          value={form.message}
+          onChange={onChange}
+          className={`${INPUT} resize-none`}
+        />
+        <button className="rounded-md bg-gray-100 text-gray-800">Submit</button>
+      </form>
+    </>
+  );
 }
